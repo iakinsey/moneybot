0fae47baba54bd6c744e0a0d14fbb4cc279c86ba
iakinsey <ik.ghub.1@nym.hush.com>
2016-09-05 15:27:39 -0700

    Discord key environment variable

--- moneybot/config.py
+++ moneybot/config.py
@@ -1,4 +1,15 @@
+from os import environ
 from os.path import dirname, realpath, join, abspath
+
+
+def required(key):
+    val = environ.get(key)
+
+    if not val:
+        print("Missing environment variable: '{}'".format(key))
+        exit(1)
+
+    return val
 
 
 PROJECT_ROOT = abspath(dirname(dirname(realpath(__file__))))
@@ -7,4 +18,4 @@ DB_PATH = join(DATA_PATH, 'ledger.db')
 SQL_PATH = join(PROJECT_ROOT, 'sql')
 SCHEMA_FILE_NAME = 'schema.sql'
 SCHEMA_FILE_PATH = join(SQL_PATH, SCHEMA_FILE_NAME)
-DISCORD_TOKEN = ""
+DISCORD_TOKEN = required("MONEYBOT_APP_KEY")
